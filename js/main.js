704a88abdf8cf9b7830959f499c86f8e09bb703b
MichaelJohnJacob <michaljanjakub@o2.pl>
2023-03-09 16:19:27 +0100

    Added scroll spy function

--- js/main.js
+++ js/main.js
@@ -2,12 +2,30 @@ const menuIcon = document.querySelector('.navbar__icon')
 const navBar = document.querySelector('.navbar')
 const navMenu = document.querySelector('.navbar__list')
 const navLinks = document.querySelectorAll('.navbar__list-item')
+const scrollSpySections = document.querySelectorAll('.section')
 
 const changeMenuWidth = () => {
 	if (window.scrollY > 20) {
 		navBar.classList.add('navbar__active')
 	} else {
 		navBar.classList.remove('navbar__active')
+	}
+}
+
+const handleScrollSpy = () => {
+	if (document.body.classList.contains('main-page')) {
+		const sections = []
+
+		scrollSpySections.forEach(section => {
+			if (window.scrollY <= section.offsetTop + section.offsetHeight - 103) {
+				sections.push(section)
+				const activeSection = document.querySelector(`[href*="${sections[0].id}"]`)
+
+				navLinks.forEach(item => item.classList.remove('navbar__list-item--active'))
+
+				activeSection.classList.add('navbar__list-item--active')
+			}
+		})
 	}
 }
 
@@ -26,3 +44,4 @@ navLinks.forEach(n =>
 )
 
 window.addEventListener('scroll', changeMenuWidth)
+window.addEventListener('scroll', handleScrollSpy)
